5d75d5c0a17fe4e05e12585da52d4ffe2c15b7d4
guichag <160218486+guichag@users.noreply.github.com>
2024-10-06 20:44:52 +0100

    Add files via upload

---
+++ fdt/fdt_config.py
@@ -0,0 +1,9 @@
+"""CMIP6 NS-GEV data config file"""
+
+import os
+
+DIRNAME = os.path.dirname(__file__)
+
+OUTDIR = os.path.join(DIRNAME, "output")
+DATADIR = os.path.join(OUTDIR, "data")
+FIGDIR = os.path.join(OUTDIR, "figures")
